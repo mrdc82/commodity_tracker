ca2ac1175c7a4af39be78a74c9a04f2c54476bfc
Dino Charalambous <dinochara@gmail.com>
2023-04-07 16:54:10 +0200

    fix

--- commodity_tracker.py
+++ commodity_tracker.py
@@ -1,28 +1,18 @@
 '''
-<<<<<<< HEAD
 Author: Me and ChatGpt
-=======
-Author: Me and ChatGpt"
->>>>>>> 9fc941d00842dda048e0152b7f6900d07cac5c72
 Date: 07-April-2023"
 Description: An API request to yahoo finance which will collect all historical data for dates specified, and print out the best months
 to buy and sell a commodity. Please refer to yahoo finance for correct symbol names.
 '''
 
-<<<<<<< HEAD
 print("An API request to yahoo finance which will collect all historical data for dates specified, \nand print out the best months\
  to buy and sell a commodity. Please refer to yahoo finance for correct symbol names")
 
-=======
->>>>>>> 9fc941d00842dda048e0152b7f6900d07cac5c72
 import requests
 import statistics
 from datetime import date, datetime
 import time
-<<<<<<< HEAD
 from getpass import getpass
-=======
->>>>>>> 9fc941d00842dda048e0152b7f6900d07cac5c72
 
 symbol = input("Symbol: ") # symbol to retreive data for, ie. ^XAU
 
@@ -38,19 +28,12 @@ eday = int(input('Enter a end day: '))
 ed = date(eyear, emonth, eday)
 period2 = int(time.mktime(ed.timetuple()))
 
-<<<<<<< HEAD
 api_key = getpass("enter API-KEY *hidden from view*: ")
 
 url = f"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data?symbol={symbol}&period1={period1}&period2={period2}&frequency=1mo"
 
 headers = {
     "X-RapidAPI-Key": (f"{api_key}"),
-=======
-url = f"https://apidojo-yahoo-finance-v1.p.rapidapi.com/stock/v2/get-historical-data?symbol={symbol}&period1={period1}&period2={period2}&frequency=1mo"
-
-headers = {
-    "X-RapidAPI-Key": input("enter API-KEY: "),
->>>>>>> 9fc941d00842dda048e0152b7f6900d07cac5c72
     "X-RapidAPI-Host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
 }
 
@@ -79,7 +62,3 @@ if response.status_code == 200:
     print(f"\nHistorically, the highest average price has been in month {highest_month}, and the lowest average price has been in month {lowest_month}.")
 else:
     print(f"Error {response.status_code} occurred.")
-<<<<<<< HEAD
-=======
->>>>>>> 46441a0a4e75c3a6a80730ac1fd8e5ae27b0cc06
->>>>>>> 9fc941d00842dda048e0152b7f6900d07cac5c72
